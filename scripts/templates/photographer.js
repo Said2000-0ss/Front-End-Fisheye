c107b88038200a03b68b93d9070dbf8fba2c39b7
Manssouri <saidmanssouri@gmail.com>
2024-06-14 13:26:29 +0200

    Mise-en-place-de-la-recuperation-des-données-dans-la-fonction-photographerTemplate

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,7 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    // Ajout des donnees 
+    // const { name, portrait } = data;
+    const { name,id, city, country, tagline, price ,  portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,9 +11,51 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        //=============================================================================================================================================
+        const p = document.createElement( 'p' );
+        const p1 = document.createElement( 'p' );
+        const p2 = document.createElement( 'p' );
+        const p3 = document.createElement( 'p' );
+        const p4 = document.createElement( 'p' );
+        p.textContent = id;
+        p1.textContent=city;
+        p2.textContent=country;
+        p3.textContent=tagline;
+        p4.textContent=price;
+        //=============================================================================================================================================
         article.appendChild(img);
         article.appendChild(h2);
+        //=============================================================================================================================================
+        article.appendChild(p);
+        article.appendChild(p1);
+        article.appendChild(p2);
+        article.appendChild(p3);
+        article.appendChild(p4);
+        console.log(name + " "+ id + " "+ city+ " "+  country+ " "+  tagline+ " "+  price + " "+ portrait )
+        //=============================================================================================================================================
         return (article);
     }
     return { name, picture, getUserCardDOM }
 }
+//==============================================================================================================================
+// function photographerTemplate(data) {
+//     // Ajout des donnees 
+//     // const { name, portrait } = data;
+//     const { name, portrait } = data;
+
+//     const picture = `assets/photographers/${portrait}`;
+
+//     function getUserCardDOM() {
+//         const article = document.createElement( 'article' );
+//         const img = document.createElement( 'img' );
+//         img.setAttribute("src", picture)
+//         const h2 = document.createElement( 'h2' );
+//         h2.textContent = name;
+        
+//         article.appendChild(img);
+//         article.appendChild(h2);
+      
+//         return (article);
+//     }
+//     return { name, picture, getUserCardDOM }
+// }
